df2e71ef57b6e16fb49dda92c61aecfa792ebcf6
ekim <eunkukkim1992@gmail.com>
2022-03-11 13:19:48 +0900

    headers

--- eunflex/src/commons/layout/header/header.presenter.tsx
+++ eunflex/src/commons/layout/header/header.presenter.tsx
@@ -1,7 +1,33 @@
+import * as S from "./header.styles";
+import "antd/dist/antd.css";
+import { Menu } from "antd";
+import { DownOutlined } from "@ant-design/icons";
+
 export default function HeaderUI() {
+  const menu = (
+    <Menu>
+      <Menu.Item key="0">
+        <div>한국어</div>
+      </Menu.Item>
+      <Menu.Item key="1">
+        <div>English</div>
+      </Menu.Item>
+      <Menu.Divider />
+    </Menu>
+  );
+
   return (
-    <>
-      <div>Hello world</div>
-    </>
+    <S.Wrapper>
+      <img src="/netflix.png" />
+      <S.Language>
+        <S.WorldImage src="/world.png/" />
+        <S.Breadcrumb overlay={menu} trigger={["click"]}>
+          <a className="ant-dropdown-link" onClick={(e) => e.preventDefault()}>
+            English <DownOutlined />
+          </a>
+        </S.Breadcrumb>
+      </S.Language>
+      <button>Sign In</button>
+    </S.Wrapper>
   );
 }
--- eunflex/src/commons/layout/header/header.styles.ts
+++ eunflex/src/commons/layout/header/header.styles.ts
@@ -1,5 +1,24 @@
 import styled from "@emotion/styled";
+import { Dropdown } from "antd";
 
 export const Wrapper = styled.div`
   margin: 0px auto;
+  background-color: khaki;
+`;
+
+export const Language = styled.span`
+  border: 2px solid black;
+  background-color: lightcoral;
+`;
+
+export const Breadcrumb = styled(Dropdown)`
+  width: 120px;
+  height: 120px;
+  /* background-color: lightblue; */
+  color: #ffffff;
+`;
+
+export const WorldImage = styled.img`
+  width: 32px;
+  height: 32px;
 `;
