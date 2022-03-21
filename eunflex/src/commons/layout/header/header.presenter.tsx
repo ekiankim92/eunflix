c9173924d42cb413fdae2a48e8f65b67105c282b
ekim <eunkukkim1992@gmail.com>
2022-03-21 16:35:24 +0900

    headers update

--- eunflex/src/commons/layout/header/header.presenter.tsx
+++ eunflex/src/commons/layout/header/header.presenter.tsx
@@ -4,15 +4,19 @@ export default function HeaderUI() {
   return (
     <S.Wrapper>
       <S.NetflixImage src="/netflix.png/" />
-      <S.Home>홈</S.Home>
-      <div>시리즈</div>
-      <div>영화</div>
-      <div>NEW! 요즘 대세 콘텐츠</div>
-      <div>내가 찜한 콘텐츠</div>
-      <img src="/search.png/" />
-      <div>키즈</div>
-      <img src="/bell.png/" />
-      <div>User information</div>
+      <S.ContentsWrapper>
+        <S.Home>홈</S.Home>
+        <S.Series>시리즈</S.Series>
+        <S.Movies>영화</S.Movies>
+        <S.Trends>NEW! 요즘 대세 콘텐츠</S.Trends>
+        <S.MyPicks>내가 찜한 콘텐츠</S.MyPicks>
+      </S.ContentsWrapper>
+      <S.IconsWrapper>
+        <S.SearchIcon src="/search.png/" style={{ color: "white" }} />
+        <S.Kids>키즈</S.Kids>
+        <S.BellIcon src="/bell.png/" style={{ color: "white" }} />
+        <S.ProfileIcons>Profile Icons</S.ProfileIcons>
+      </S.IconsWrapper>
     </S.Wrapper>
   );
 }
--- eunflex/src/commons/layout/header/header.styles.ts
+++ eunflex/src/commons/layout/header/header.styles.ts
@@ -1,17 +1,75 @@
 import styled from "@emotion/styled";
 
 export const Wrapper = styled.div`
-  display: flex;
+  width: 100%;
   height: 60px;
+  display: flex;
+  position: fixed;
   background-color: rgb(20, 20, 20);
 `;
 
 export const NetflixImage = styled.img`
+  width: 120px;
   margin-left: 30px;
-  /* padding: 8px; */
-  /* background-color: white; */
+`;
+
+export const ContentsWrapper = styled.div`
+  width: 40%;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  margin-left: 8px;
 `;
 
 export const Home = styled.div`
   color: #eee;
+`;
+
+export const Series = styled.div`
+  color: #eee;
+`;
+
+export const Movies = styled.div`
+  color: #eee;
+`;
+
+export const Trends = styled.div`
+  color: #eee;
+`;
+
+export const MyPicks = styled.div`
+  color: #eee;
+`;
+
+export const IconsWrapper = styled.div`
+  width: 60%;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+`;
+
+export const SearchIcon = styled.img`
+  width: 22px;
+  color: #eee;
+  border-radius: 3px;
+  background-color: #eee;
+  margin-right: 16px;
+`;
+
+export const Kids = styled.div`
+  color: #eee;
+  margin-right: 16px;
+`;
+
+export const BellIcon = styled.img`
+  width: 22px;
+  color: #eee;
+  border-radius: 10px;
+  background-color: #eee;
+  margin-right: 16px;
+`;
+
+export const ProfileIcons = styled.div`
+  color: #eee;
+  margin-right: 56px;
 `;
